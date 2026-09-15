5e5ed8da380526324eeddf2237e280485a1226a3
jhordanhinostroza <jhordanhinostroza8@gmail.com>
2026-09-15 13:02:47 -0500

    Update minireto.py

--- minireto.py
+++ minireto.py
@@ -1,13 +1,10 @@
-# --- Paso 1.4 Mini-reto: estadística descriptiva con NumPy ---
 import numpy as np
-
 
 temperaturas = np.array([12.5, 14.0, 11.8, 13.2, 15.1, 10.9, 12.7])
 print('Promedio:', temperaturas.mean())
 print('Desviación estándar:', temperaturas.std())
 print('Mínima:', temperaturas.min())
 print('Máxima:', temperaturas.max())        
-
 
 import time
 import numpy as np
@@ -25,9 +22,6 @@ inicio = time.time()
 resultado_array = array + 10
 print("Vectorizado:", time.time() - inicio, "segundos")
 
-
-
-# --- Paso 1.4: Mini-reto: estadística descriptiva ---
 temperaturas = np.array([12.5, 14.0, 11.8, 13.2, 15.1, 10.9, 12.7])
 print("Promedio:", temperaturas.mean())
 print("Desviación estándar:", temperaturas.std())
